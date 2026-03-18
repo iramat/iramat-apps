4cec5b18b4367557fe0f229bd3372ff6be427833
HUET Thomas <TH282424@extra.cea.fr>
2026-03-18 14:33:37 +0100

    replaced previous file by Doks ones

---
+++ deploy-gh-pages.ps1
@@ -0,0 +1,25 @@
+# run it as: .\deploy-gh-pages.ps1 "Deploy Doks site"
+
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$CommitMessage
+)
+# Stop on first error
+$ErrorActionPreference = "Stop"
+
+Write-Host "▶ Building Hugo site..."
+hugo -D --minify -d .\public --baseURL "https://iramat.github.io/iramat-apps/"
+
+Write-Host "▶ Entering public/ (gh-pages worktree)..."
+Push-Location .\public\
+
+Write-Host "▶ Git status:"
+git status
+
+Write-Host "▶ Committing and pushing to gh-pages..."
+git add .
+git commit -m $CommitMessage
+git push --force origin gh-pages
+
+Pop-Location
+Write-Host "✅ Deployment to gh-pages completed successfully."
